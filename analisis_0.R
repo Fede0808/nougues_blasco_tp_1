084027c8ffaeb6c80ccc8ffaf5a47e291e42d137
Federico Blasco <federicoblasco8@gmail.com>
2023-08-09 11:16:44 -0300

    carga librerias y bases.

---
+++ analisis_0.R
@@ -0,0 +1,9 @@
+#libreria ####
+library(readr)
+library(tidyverse)
+
+#bases ####
+w_mean_depto_total_letra <- read_csv("w_mean_depto_total_letra.csv")
+diccionario_clase2 <- read_csv('diccionario_clae2.csv')
+diccionario_cod_depto <- read_csv('diccionario_cod_depto.csv')
+
