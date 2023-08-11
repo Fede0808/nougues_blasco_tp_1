973d408a2c81ea648d2ef91668a78b2e38c31a56
tnougues <141449788+tnougues@users.noreply.github.com>
2023-08-11 16:21:07 -0300

    Add files via upload

---
+++ consigna2.R
@@ -0,0 +1,40 @@
+# Paquetes
+library(tidyverse)
+library(ggplot2)
+library(sf)
+library(dplyr)
+library(readr)
+library(viridis)
+library(scales)
+library(lubridate)
+library(cowplot)
+library(ggtext)
+library(xfun)
+library(ggrepel)
+
+##Carga de bases
+
+base <- read.csv("bases/w_mean_depto_total_letra.csv")
+head(base)
+class(base$fecha)
+
+###Consgina 2
+base_filtrada_fecha <- base %>%
+  filter(fecha =="2023-04-01")
+
+tabla1 <- base_filtrada_fecha %>% 
+  select(letra, w_mean) %>% 
+  group_by(letra) %>% 
+  summarize(mean(w_mean))
+
+grafo_media_sector <- ggplot(tabla_media_sector, aes(x = letra, y = w_mean)) +
+  geom_col() +
+  scale_y_log10(labels=scales::comma) +
+  theme_minimal() +
+  labs(title = "Media salarial por sector de actividad", 
+       subtitle = "según datos de abril 2023",
+       x = "Sector de actividad", 
+       y = "Salario promedio")
+
+print(grafo_media_sector)
+            
