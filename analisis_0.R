c0be0c6c42612a0f7cdb2906964b9dacbd47c5f4
Federico Blasco <federicoblasco8@gmail.com>
2023-08-14 22:46:02 -0300

    .

--- analisis_0.R
+++ analisis_0.R
@@ -17,12 +17,24 @@ dicc_clase2_letra <- clase2_letra[!duplicated(clase2_letra$letra), ]
   
 w_mean_total_descletra <- left_join(dicc_clase2_letra, w_mean_depto_total_letra,c("letra")) #tiene menos obs. Revisar.
 
+rm(w_mean_depto_total_letra)
 
+rm(clase2_letra)
+
+rm(diccionario_clase2)
 ##Agrego descripcion depto y provincia.####
 
 salario_total_promedio_descrip <- left_join(diccionario_cod_depto,w_mean_total_descletra,
                                             c("id_provincia_indec","codigo_departamento_indec")) #Revisar. Sigue achicando.
 
+
+salario_total_promedio_descrip <- salario_total_promedio_descrip %>% 
+  mutate(ano =  format(fecha, '%Y'))%>%
+  filter(ano != '2023', w_mean > '0')  
+  
+
+
+rm(w_mean_total_descletra)
 #analisis ####
 
 ##Los 5 sectores de actividad con salarios más bajos, expresados en un gráfico de barras.####
@@ -30,6 +42,53 @@ salario_total_promedio_descrip <- left_join(diccionario_cod_depto,w_mean_total_d
 ##Elija 4 sectores de actividad (los cuales se distinguen por la letra) o grupos de sectores ####
 #y visualice la evolución de los salarios a lo largo de los años disponibles. 
 
+#Elijo los 2 sectores con salario más alto y los 2 con salario más bajo.
 
-dispersion_wmean <- ggplot(salario_total_promedio_descrip,aes(x=as.Date(salario_total_promedio_descrip$fecha, format= '%Y'),y= w_mean))+
-  geom_point()
+top5_salario_prom <- salario_total_promedio_descrip %>%
+  group_by(ano,letra_desc) %>%
+  summarise(salario = mean(w_mean)) %>%
+  arrange(desc(salario)) %>%
+  head(5)
+
+alta_remu <- c('EXPLOTACION DE MINAS Y CANTERA',
+' SUMINISTRO DE ELECTRICIDAD, GAS, VAPOR Y AIRE ACONDICIONADO')
+
+ult5_salario_prom <- salario_total_promedio_descrip %>%
+  filter( ano == '2022') %>% 
+  group_by(ano,letra_desc) %>%
+  summarise(salario = mean(w_mean)) %>%
+  tail(5) %>% 
+  arrange(salario)
+
+muestra <- c('EXPLOTACION DE MINAS Y CANTERA',
+               ' SUMINISTRO DE ELECTRICIDAD, GAS, VAPOR Y AIRE ACONDICIONADO',
+               'SERVICIOS INMOBILIARIOS',
+               'SERVICIOS DE ASOCIACIONES Y SERVICIOS PERSONALES')
+
+
+dispersion_wmean <- ggplot(salario_total_promedio_descrip,
+                           aes(x=as.Date(fecha,
+                                             format= '%Y'),y= w_mean))+
+  geom_point()+
+  scale_y_log10(labels=scales::comma)
+
+ggsave('dispersion_wmean.png',plot = dispersion_wmean,dpi = 300)
+
+#agrupo por actividad a nivel nacional. 
+
+sal_prom_nacional <- salario_total_promedio_descrip %>% 
+  filter( letra_desc == 'EXPLOTACION DE MINAS Y CANTERA'|
+          ' SUMINISTRO DE ELECTRICIDAD, GAS, VAPOR Y AIRE ACONDICIONADO'|
+          'SERVICIOS INMOBILIARIOS'|
+          'SERVICIOS DE ASOCIACIONES Y SERVICIOS PERSONALES' ) %>% 
+  group_by(fecha,letra_desc) %>% 
+  summarise(salario = mean(w_mean))
+
+dispersion_wmean_4 <- ggplot(sal_prom_nacional,
+                             aes(x=as.Date(fecha,format= '%Y'),y= salario))+
+                                                  
+  geom_point()+
+  scale_y_log10(labels=scales::comma)
+
+ggsave('dispecion_4a.png', plot = dispersion_wmean_4, dpi = 300) #¿habria que agrupar a nivel pais?
+
